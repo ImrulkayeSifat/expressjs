af8d60a8dd55dc2e4407945203e94c5a7075e28e
ImrulkayeSifat <kayesimrul224@gmail.com>
2022-09-02 18:22:00 +0600

    first commit

---
+++ routes/products.js
@@ -0,0 +1,38 @@
+const router = require('express').Router();
+const ErrorHandler = require('../errors/ErrorHandler');
+let products = require('../productData');
+const apiKeyMiddleware = require('../middlewares/apiKey');
+
+router.get('/products', (req, res) => {
+    res.render('products', {
+        title: 'My Product page'
+    });
+});
+
+router.get('/api/products', (req,res) => {
+    res.json(products);
+});
+
+router.post('/api/products', (req, res, next) => {
+
+
+    const { name, price } = req.body;
+
+    if (!name || !price) {
+        next(ErrorHandler.validationError('Name and price fields are required!'));
+    }
+    const product = {
+        name,
+        price,
+        id: new Date().getTime().toString()
+    }
+    products.push(product);
+    res.json(product);
+});
+
+router.delete('/api/products/:productId', (req, res) => {
+    products = products.filter((product) => req.params.productId !== product.id);
+    res.json({ status: 'OK' });
+});
+
+module.exports = router;
